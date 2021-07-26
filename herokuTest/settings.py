667ca41c7109c54d267bc4377de72bb833230ecf
zablonrioba <riobazablon@gmail.com>
2021-07-26 21:45:18 +0300

    update staticfiles

--- herokuTest/settings.py
+++ herokuTest/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
